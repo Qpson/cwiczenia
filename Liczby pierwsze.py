97b7dc2b0653a171c4a9685f13907be24284336d
Qpson <jakub.dmuchowski@o2.pl>
2018-04-27 09:17:04 +0200

    Znowu halo

--- Liczby pierwsze.py
+++ Liczby pierwsze.py
@@ -1,15 +1,16 @@
-dol = int(input('Podaj pierwsza liczbe, od jakiej chcesz sprawdzac: '))
-gora = int(input('Podaj ostatnia liczbe, do ktorej chcesz sprawdzac: '))
+def liczbyPierwsze(dolnaGranica, gornaGranica):
+    print("Liczby pierwsze od ",dolnaGranica,"do",gornaGranica,"to:")
+    for liczba in range(dolnaGranica,gornaGranica + 1):
+        if liczba> 1:
+            for i in range(2,liczba):
+                if (liczba % i) == 0:
+                    break
+            else:
+                print(liczba)
 
+def main():
+    dolnaGranica = int(input('Podaj pierwsza liczbe, od jakiej chcesz sprawdzac: '))
+    gornaGranica = int(input('Podaj ostatnia liczbe, do ktorej chcesz sprawdzac: '))
+    print(liczbyPierwsze(dolnaGranica, gornaGranica))
 
-
-print("Liczby pierwsze od ",dol,"do",gora,"to:")
-
-for liczba in range(dol,gora + 1):
-   
-   if liczba> 1:
-       for i in range(2,liczba):
-           if (liczba % i) == 0:
-               break
-       else:
-           print(liczba)
+main()
---
+++ test2.py
@@ -0,0 +1,8 @@
+def potega4(x):
+    return x*x*x*x
+
+def main():
+     x = input('Podaj liczbe, ktora chcesz podniesc do potegi 4: ')
+    print(potega4(x))
+
+main()
